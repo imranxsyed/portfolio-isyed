7d47302e6cc3fb6420adb2d3c35e9893dc682e67
Imran <imranxsyed@hotmail.com>
2020-03-30 02:12:16 -0400

    Added static files configuration

---
+++ staticfiles/portfolio/assets/js/index.c238fc306bf2.js
@@ -0,0 +1,35 @@
+
+			$(document).ready(function(){
+				
+				$('#display-picture').animate({top:"0"},1000);
+				$('#intro').animate({left:"0"},1000);
+				$('#qualification').animate({right:"0"},1000);
+				$('#about-me').animate({left:"0"},1000);
+			
+				
+			});
+		function btnSendEmail(){
+			
+			//cleaning up errors
+			$('#invalid-email').hide();
+			$('#invalid-message').hide();
+			var message = $("#modal-message").val();
+			var email_address = $("#modal-email-address").val();
+			if (email_address == null || email_address == "" || !email_address.includes("@")){
+				$('#invalid-email').show();
+				return;
+			}
+			if (message == null || message == ""){
+				$('#invalid-message').show();
+				return;
+			}
+			//cleaning up fields
+			$("#modal-message").val("");
+			$("#modal-email-address").val("");
+			//cleaning up errors
+			$('#invalid-email').hide();
+			$('#invalid-message').hide();
+			//closing the model
+			$('#btn-close-modal').click();
+		}
+			
