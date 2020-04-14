f1121de2537de6c64f1959430cde70dee7df71f0
Imran <imranxsyed@hotmail.com>
2020-04-14 04:31:55 -0400

    Connected all the downloaded files with load static - moved css to style.css - moved script to index.js

--- portfolio/static/portfolio/assets/js/index.js
+++ portfolio/static/portfolio/assets/js/index.js
@@ -1,37 +1,24 @@
 
-			$(document).ready(function(){
-				
-				
-				$('#display-picture').animate({top:"0"},1000);
-				
-				$('#qualification').animate({right:"0"},1000);
-				$('#about-me').animate({left:"0"},1000);
+$(document).ready(function(){
 
-			
-				
-			});
-		function btnSendEmail(){
-			
-			//cleaning up errors
-			$('#invalid-email').hide();
-			$('#invalid-message').hide();
-			var message = $("#modal-message").val();
-			var email_address = $("#modal-email-address").val();
-			if (email_address == null || email_address == "" || !email_address.includes("@")){
-				$('#invalid-email').show();
-				return;
-			}
-			if (message == null || message == ""){
-				$('#invalid-message').show();
-				return;
-			}
-			//cleaning up fields
-			$("#modal-message").val("");
-			$("#modal-email-address").val("");
-			//cleaning up errors
-			$('#invalid-email').hide();
-			$('#invalid-message').hide();
-			//closing the model
-			$('#btn-close-modal').click();
-		}
-			
+	$(".card").mouseenter( function(){
+		$(this).children(".card-text").show(200);
+	} ).mouseleave( function(){
+		$(this).children(".card-text").hide(200);
+	} );	
+
+
+});
+
+window.onload = function() {
+	Particles.init({
+		selector: '.background',
+		color: '#ffffff',
+		connectParticles: true,
+		maxParticles: 150,
+
+
+
+	});
+};
+
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+
 def index(request):
     #return render(request,"portfolio/index.html")
     return render(request,"portfolio/test.html")
