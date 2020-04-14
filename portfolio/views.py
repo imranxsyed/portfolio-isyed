7a4a9b3322900afa626aefc18361095173058627
Imran <imranxsyed@hotmail.com>
2020-04-14 04:40:14 -0400

    moved test.html to index.html

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,5 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    #return render(request,"portfolio/index.html")
-    return render(request,"portfolio/test.html")
+    return render(request,"portfolio/index.html")
